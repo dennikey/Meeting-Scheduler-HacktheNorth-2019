5899f474be114ebf226ca97d0ca9f831b03da54c
Kevin Xu <42011894+kevinxyc1@users.noreply.github.com>
2019-09-14 15:04:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var bodyParser = require('body-parser');
 
 // viewed at http://localhost:8080
+app.use(bodyParser.urlencoded({extended : true}));
+app.set("view engine", "ejs");
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.render('/index.html');
 });
 
 app.post('/people', function (req, res) {
-	console.log(req.params)
+	// 1. Save data
+	// 2. show people page
+	console.log(req.body.firstname)
+});
+
+app.post('/addfriend', function (req, res) {
+	console.log(req.body.firstname)
 });
 
 app.listen(8080, function () { console.log("Listening")});
