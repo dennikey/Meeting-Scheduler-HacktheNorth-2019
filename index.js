715c0825c9349aa6811f35604f38150f8fb4feb6
Kevin Xu <42011894+kevinxyc1@users.noreply.github.com>
2019-09-15 01:10:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,81 +1,68 @@
-const form = document.getElementById('info-form');
-
-function renderInfo(doc){
-	let li = document.createElement('li');
-	let firstname = document.createElement('span');
-	let lastname =  document.createElement('span');
-	
-	li.setAttribute('data-id', doc.id)
-}
-
-db.collection('userinfo').get().then((snapshot) => {
-	snapshot.docs.forEach(doc => {
-		renderInfo(doc);
-	})
-})
-
-form.addEventListener("submit", (e) => {
-	e.preventDefault();
-	
-	firebase.firestore.collection("userinfo").add({
-		firstname: firstname.value,
-		lastname: lastname.value,
-		email: email.value,
-		facebook: facebook.value,
-		instagram: instagram.value,
-		wechat: wechat.value,
-		english: english.selected,
-		math: math.selected,
-		science: science.selected,
-		soccer: soccer.selected,
-		basketball: basketball.selected
-	})	
-	
-	return false;
-});
-
-/*var express = require('express');
+var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var path = require('path');
-var bodyParser = require('body-parser');
+var admin = require('firebase-admin');
+var firebase = require('firebase');
 
+var config = {
+    apiKey: "AIzaSyDjkhHIpFL7PSBlTte09ywFCtZnspNooUQ",
+    authDomain: "uwloo-connect.firebaseapp.com",
+    databaseURL: "https://uwloo-connect.firebaseio.com",
+    projectId: "uwloo-connect",
+    storageBucket: "uwloo-connect.appspot.com",
+    messagingSenderId: "491763611401"
+};
 
+firebase.initializeApp(config);
+var db = firebase.firestore();
+
+app.use(express.static(__dirname));
+app.use(bodyParser());
 
   // ...
-
 // viewed at http://localhost:8080
-/*app.use(bodyParser.urlencoded({extended : true}));
-app.set("view engine", "ejs");
+// app.set("view engine", "ejs");
 app.get('/', function(req, res) {
-    //res.render('/index.html');
-    //res.send("hello");
-    console.log('get request');
-    res.sendFile('index.html',{
-    	root: path.join(__dirname, './')
-    })
+	res.render('./index.html')
 });
 
-/*app.post('/people.html', function (req, res) {
+app.post('/userinfo', function (req, res) {
 	// 1. Save data
 	// 2. show people page
-	console.log(req.body.firstname)
-	console.log(req.body.lastname)
-	console.log(req.body.email)
-	console.log(req.body.facebook)
-	console.log(req.body.instagram)
-	console.log(req.body.wechat)
-	console.log(req.body.english)
-	console.log(req.body.math)
-	console.log(req.body.science)
-	console.log(req.body.soccer)
-	console.log(req.body.basketball)
-
+	console.log(req.body)
+	db.collection("userinfo").add({
+    	firstname: req.body.firstname,
+    	lastname: req.body.lastname,
+    	email: req.body.email,
+    	facebook: req.body.facebook,
+    	instagram: req.body.instagram,
+    	wechat: req.body.wechat,
+   		subjects: req.body.subjects,
+    	sports: req.body.sports
+	})
+	.then(function(docRef) {
+    	console.log("Document written with ID: ", docRef.id);
+	})
+	.catch(function(error) {
+    	console.error("Error adding document: ", error);
+	});
+	// console.log(req.body.lastname)
+	// console.log(req.body.email)
+	// console.log(req.body.phonenumber)
+	// console.log(req.body.facebook)
+	// console.log(req.body.instagram)
+	// console.log(req.body.wechat)
+	// console.log(req.body.science)
+	// console.log(req.body.soccer)
+	// console.log(req.body.basketball)
 	//db.push();
-	
+	res.end();
 });
 
 /*
-db.collection("userinfo").add({
+function addMockData() {
+	db.collection("userinfo").add({
     firstname: "Hey",
     lastname: "Lovelace",
     email: "dsfadsf",
@@ -87,7 +74,6 @@ db.collection("userinfo").add({
     science: false,
     soccer: true,
     basketball: true
-
 })
 .then(function(docRef) {
     console.log("Document written with ID: ", docRef.id);
@@ -95,10 +81,12 @@ db.collection("userinfo").add({
 .catch(function(error) {
     console.error("Error adding document: ", error);
 });
+
+}
 */
 
 
-
-/*app.listen(8080, () =>{
+app.listen(8080, () =>{
 	console.log(`server running on port 8080`)
 });
+
